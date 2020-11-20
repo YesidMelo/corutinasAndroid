5f4bf00a1966e46dbfbd813cd769758f5c3cc5ba
YesidMelo <emelo@robinfood.com>
2020-11-20 14:59:11 -0500

    ejemplo de bloqueo

--- app/src/main/java/com/mitiempo/aprendizajecorutinas/MainActivity.kt
+++ app/src/main/java/com/mitiempo/aprendizajecorutinas/MainActivity.kt
@@ -2,10 +2,31 @@ package com.mitiempo.aprendizajecorutinas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        blockingExample()
+
     }
+
+    //region bloqueo
+    fun longTaskWithMessage(mensaje : String){
+        Thread.sleep(4_000)
+        Log.e("Mensaje", mensaje +" " +Thread.currentThread().name)
+
+    }
+
+    fun blockingExample(){
+        Log.e("Mensaje", "Tarea 1" +" " +Thread.currentThread().name)
+        longTaskWithMessage("Tarea 2")
+        Log.e("Mensaje", "Tarea 3" +" " +Thread.currentThread().name)
+    }
+    //endregion
+
 }
