cdf9737f63300e04b8cf29a3b59d48b0cac5c3e9
Yesid <trigoperfec3@hotmail.com>
2020-11-22 10:50:17 -0500

    Correccion en la anotacion de la interface MotorComponent usando la anotacion singleton

--- app/src/main/java/com/mitiempo/aprendizajecorutinas/Dagger/DI/MotorComponent.kt
+++ app/src/main/java/com/mitiempo/aprendizajecorutinas/Dagger/DI/MotorComponent.kt
@@ -2,8 +2,10 @@ package com.mitiempo.aprendizajecorutinas.Dagger.DI
 
 import com.mitiempo.aprendizajecorutinas.Dagger.external.Activities.MainActivityDagger
 import dagger.Component
+import javax.inject.Singleton
 
 @PerActivity
+@Singleton
 @Component(modules = arrayOf(MotorModule::class))
 interface MotorComponent {
     fun inject(mainActivityDagger: MainActivityDagger)
