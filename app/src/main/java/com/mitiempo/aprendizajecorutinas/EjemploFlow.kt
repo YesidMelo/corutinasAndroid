a1ec696ef73d8ffc24644f61c15e5000d9b3c666
Yesid <trigoperfec3@hotmail.com>
2020-11-21 19:03:47 -0500

    introduccion primer flow

--- app/src/main/java/com/mitiempo/aprendizajecorutinas/EjemploFlow.kt
+++ app/src/main/java/com/mitiempo/aprendizajecorutinas/EjemploFlow.kt
@@ -3,6 +3,12 @@ package com.mitiempo.aprendizajecorutinas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class EjemploFlow : AppCompatActivity() {
     fun String.imprimirEnConsola(){
@@ -11,7 +17,26 @@ class EjemploFlow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejemplo_flow)
-        show()
+        /*
+//        show()
+        runBlocking {
+            runAsynchronous().forEach { i -> " $i".imprimirEnConsola() }
+        }
+         */
+        runBlocking {
+
+            launch {
+                for (j in 1 .. 40){
+                    "no estoy bloqueado $j".imprimirEnConsola()
+                    delay(1_000)
+                }
+            }
+
+            firstFlow()
+                    .collect {//esta linea es la que recoge el valor generado por el emit del flow
+                value -> "valor : $value".imprimirEnConsola()
+            }
+        }
     }
     //region introduccion (se bloquea la pantalla por 3 segundos la idea es realizar operaciones de manera asincrona)
     //region ejemplo listar
@@ -29,5 +54,25 @@ class EjemploFlow : AppCompatActivity() {
         }
     }
     //endregon
+    //endregion
+
+    //region manejo con corutinas
+    suspend fun runAsynchronous() : List<Int>{
+        return runBlocking {
+            delay(1_000)
+            return@runBlocking listar()
+        }
+    }
+    //endregion
+    /*
+        flow : es un callback que emite valores secuencialmente.
+     */
+    //region primer flow
+    fun firstFlow() : Flow<Int> = flow{
+        for (i in 1 .. 40){
+            delay(1_000)
+            emit(i) // esta linea es la que devolvera un valor en esta funcion de corutina
+        }
+    }
     //endregion
 }
