e89b80f2807e5f05d0736e11f82ed435a4b456ce
Yesid <trigoperfec3@hotmail.com>
2020-11-26 09:01:27 -0500

    Configuracion de ui de la aplicacion

---
+++ app/src/main/java/com/mitiempo/aprendizajecorutinas/Dagger/ProyectoReal/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.mitiempo.aprendizajecorutinas.Dagger.ProyectoReal.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mitiempo.aprendizajecorutinas.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
